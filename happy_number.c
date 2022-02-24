86030c12627f9e32f804f6718e5228e50a8d8793
21p31a04a7 <99956752+21p31a04a7@users.noreply.github.com>
2022-02-24 10:40:15 +0530

    #include<stdio.h> int main() {     int i,j,N,temp,sum=0;     scanf("%d",&N);     while(sum!=1&& sum!=4)     {         sum=0;         while(N>0)         {             j=N%10;             sum+=(j*j);             N=N/10;         }         N=sum;     }     if(sum==1)     printf("True");     else     printf("False");     return 0; }

---
+++ happy_number.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+int main()
+{
+    int i,j,N,temp,sum=0;
+    scanf("%d",&N);
+    while(sum!=1&& sum!=4)
+    {
+        sum=0;
+        while(N>0)
+        {
+            j=N%10;
+            sum+=(j*j);
+            N=N/10;
+        }
+        N=sum;
+    }
+    if(sum==1)
+    printf("True");
+    else
+    printf("False");
+    return 0;
+}
